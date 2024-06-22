68d5ddd6650a760dfd45c1195a3b0dd11cf113bd
zamir5895 <lmr293141@gmail.com>
2024-06-22 11:11:03 -0500

    Implementado Seguridad

--- rimacBienestar/src/main/java/com/example/rimacbienestar/Usuarios/Infrastructure/UserRepository.java
+++ rimacBienestar/src/main/java/com/example/rimacbienestar/Usuarios/Infrastructure/UserRepository.java
@@ -1,12 +1,11 @@
 package com.example.rimacbienestar.Usuarios.Infrastructure;
 
+import com.example.rimacbienestar.Usuarios.Domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetails;
+
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer>{
-    Optional<User> findByDNI(Integer dni);
+    Optional<User> findByDni(Integer dni);
     Optional<User> findByEmail(String email);
 }
